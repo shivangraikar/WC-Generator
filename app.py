c565dff899be159b88a337bf92a9be8f5f146017
shivangraikar <51766272+shivangraikar@users.noreply.github.com>
2020-07-01 14:15:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,22 +19,38 @@ def wcg():
         count = request.form["count"]
         height = request.form["height"]
         width = request.form["width"]
+        minfontsize = request.form["minfontsize"]
+        maxfontsize = request.form["maxfontsize"]
+    
+        count = int(count)
+        width = int(width)
+        height = int(height)
+        minfontsize = int(minfontsize)
+        maxfontsize = int(maxfontsize)
+        
         title = wikipedia.search(words)[0]
         page = wikipedia.page(title)
-        text = page.content
-        print(text)
+     
+        print(page.content)
         background = np.array(Image.open("cloud.png"))
         stopwords = set(STOPWORDS)
 
-        wc = WordCloud(background_color="white",
+        wc = WordCloud(background_color="black",
                     max_words=count,
                     width = width,
+                    min_font_size = minfontsize,
+                    max_font_size = maxfontsize,
                     height = height,
                     mask = background,
                     stopwords = stopwords)
         
-        wc.generate(text)        
+        wc.generate(page.content)
         wc.to_file("wordcloud.png")
+
+        filename = Image.open("wordcloud.png")
+        filename.show()
+        
+        return render_template('wcg.html')
     else:
         return render_template('wcg.html')
 
@@ -48,5 +64,3 @@ def contact():
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
